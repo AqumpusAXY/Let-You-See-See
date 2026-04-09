65fe614985f5bb2d39900956f4c62bb2b1f5837a
AqumpusAXY <whitefroth@163.com>
2026-04-09 19:38:56 +0800

    [init]: Initial commit.

---
+++ src/main/java/github/aqumpusaxy/showmewhatyougot/ShowMeWhatYouGot.java
@@ -0,0 +1,14 @@
+package github.aqumpusaxy.showmewhatyougot;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.fml.common.Mod;
+import org.slf4j.Logger;
+
+@Mod(ShowMeWhatYouGot.MODID)
+public class ShowMeWhatYouGot {
+    public static final String MODID = "showmewhatyougot";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public ShowMeWhatYouGot() {
+    }
+}
