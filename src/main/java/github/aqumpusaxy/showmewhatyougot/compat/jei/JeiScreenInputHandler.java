ffbfd677ee63be6853852f4682d758791b20f0c7
AqumpusAXY <whitefroth@163.com>
2026-04-13 19:44:38 +0800

    [feat]: 增加对Jei配方界面物品的兼容

--- src/main/java/github/aqumpusaxy/showmewhatyougot/compat/jei/JeiScreenInputHandler.java
+++ src/main/java/github/aqumpusaxy/showmewhatyougot/compat/jei/JeiScreenInputHandler.java
@@ -2,15 +2,22 @@ package github.aqumpusaxy.showmewhatyougot.compat.jei;
 
 import github.aqumpusaxy.showmewhatyougot.network.SMWYGNetworkManager;
 import github.aqumpusaxy.showmewhatyougot.network.ShowItemPacket;
+import mezz.jei.api.constants.VanillaTypes;
+import mezz.jei.api.ingredients.IIngredientType;
 import mezz.jei.api.ingredients.ITypedIngredient;
-import mezz.jei.api.runtime.IBookmarkOverlay;
-import mezz.jei.api.runtime.IIngredientListOverlay;
-import mezz.jei.api.runtime.IJeiRuntime;
+import mezz.jei.api.runtime.*;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+
+import java.util.Collection;
 
 public class JeiScreenInputHandler {
     private static IJeiRuntime jeiRuntime;
     private static IIngredientListOverlay ingredientListOverlay;
     private static IBookmarkOverlay bookmarkOverlay;
+    private static IRecipesGui recipesGui;
+    private static IIngredientManager ingredientManager;
+    private static Collection<IIngredientType<?>> ingredientTypes;
 
     public static void setJeiRuntime(IJeiRuntime runtime) {
         jeiRuntime = runtime;
@@ -19,6 +26,9 @@ public class JeiScreenInputHandler {
     public static void init() {
         ingredientListOverlay = jeiRuntime.getIngredientListOverlay();
         bookmarkOverlay = jeiRuntime.getBookmarkOverlay();
+        recipesGui = jeiRuntime.getRecipesGui();
+        ingredientManager = jeiRuntime.getIngredientManager();
+        ingredientTypes = ingredientManager.getRegisteredIngredientTypes();
     }
 
     public static void handleJeiInput() {
@@ -34,6 +44,11 @@ public class JeiScreenInputHandler {
                     .flatMap(ITypedIngredient::getItemStack)
                     .ifPresent(itemStack -> SMWYGNetworkManager.INSTANCE.sendToServer(new ShowItemPacket(itemStack)));
 
+        }
+
+        if (recipesGui != null) {
+            recipesGui.getIngredientUnderMouse(VanillaTypes.ITEM_STACK)
+                    .ifPresent(itemStack -> SMWYGNetworkManager.INSTANCE.sendToServer(new ShowItemPacket(itemStack)));
         }
 
         //TODO:兼容JEI的其他Type, 点击聊天栏中的物品添加到书签
---
+++ src/main/java/github/aqumpusaxy/showmewhatyougot/compat/jei/package-info.java
@@ -0,0 +1,7 @@
+@ParametersAreNonnullByDefault
+@MethodsReturnNonnullByDefault
+package github.aqumpusaxy.showmewhatyougot.compat.jei;
+
+import net.minecraft.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
