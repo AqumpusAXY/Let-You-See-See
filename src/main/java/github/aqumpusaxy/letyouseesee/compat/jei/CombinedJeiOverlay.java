4e1160cd0b60fb9ec22b130c51e21ecfe042e147
AqumpusAXY <whitefroth@163.com>
2026-04-20 11:32:33 +0800

    [chore]: 减少冗余代码 - 减少CombinedJeiOverlay中多余的非空检查

--- src/main/java/github/aqumpusaxy/letyouseesee/compat/jei/CombinedJeiOverlay.java
+++ src/main/java/github/aqumpusaxy/letyouseesee/compat/jei/CombinedJeiOverlay.java
@@ -10,29 +10,15 @@ import java.util.List;
 import java.util.stream.Stream;
 
 public class CombinedJeiOverlay {
-    private final IIngredientListOverlay INGREDIENT_LIST_OVERLAY;
-    private final IBookmarkOverlay BOOKMARK_OVERLAY;
-    private final IRecipesGui RECIPES_GUI;
-
     private final List<IRecipeFocusSource> RECIPE_FOCUS_SOURCES;
 
     public CombinedJeiOverlay(IIngredientListOverlay ingredientListOverlay, IBookmarkOverlay bookmarkOverlay, IRecipesGui recipesGui) {
-        INGREDIENT_LIST_OVERLAY = ingredientListOverlay;
-        BOOKMARK_OVERLAY = bookmarkOverlay;
-        RECIPES_GUI = recipesGui;
-
-        RECIPE_FOCUS_SOURCES = List.of(
-                (IRecipeFocusSource) INGREDIENT_LIST_OVERLAY,
-                (IRecipeFocusSource) BOOKMARK_OVERLAY,
-                (IRecipeFocusSource) RECIPES_GUI
-        );
+        RECIPE_FOCUS_SOURCES = Stream.of(ingredientListOverlay, bookmarkOverlay, recipesGui)
+                .map(overlay -> (IRecipeFocusSource) overlay)
+                .toList();
     }
 
     public Stream<IClickableIngredientInternal<?>> getIngredientUnderMouse(double mouseX, double mouseY) {
-        if (INGREDIENT_LIST_OVERLAY == null) return Stream.empty();
-        if (BOOKMARK_OVERLAY == null) return Stream.empty();
-        if (RECIPES_GUI == null) return Stream.empty();
-
         return RECIPE_FOCUS_SOURCES.stream()
                 .flatMap(source -> source.getIngredientUnderMouse(mouseX, mouseY));
     }
