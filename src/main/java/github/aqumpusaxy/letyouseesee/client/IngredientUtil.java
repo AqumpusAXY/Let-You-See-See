6fc2085ca94ba8a9ab4960d4138470c538cbbe34
Yugo <2183439627@qq.com>
2026-04-26 14:18:44 +0800

    [feat]: 增加指令组件

---
+++ src/main/java/github/aqumpusaxy/letyouseesee/client/IngredientUtil.java
@@ -0,0 +1,7 @@
+package github.aqumpusaxy.letyouseesee.client;
+
+public class IngredientUtil {
+    public static Object fromString(String text) {
+        return null;
+    }
+}
---
+++ src/main/java/github/aqumpusaxy/letyouseesee/client/JeiBookmarkHelper.java
@@ -0,0 +1,6 @@
+package github.aqumpusaxy.letyouseesee.client;
+
+public class JeiBookmarkHelper {
+    public static void addBookmark(Object ingredient) {
+    }
+}
